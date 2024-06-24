862c609b7d6659af6b9d2234cb94092a6fdb3b8e
panicbit <panicbit.dev@gmail.com>
2024-06-24 23:13:10 +0200

    move varint i/o into submodule

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,8 +1,10 @@
 use std::io::{Cursor, Read, Write};
 
-use anyhow::{bail, ensure, Result};
+use anyhow::{ensure, Result};
 
-mod status_response;
+pub mod status_response;
+pub mod varint;
+
 pub use status_response::StatusResponse;
 
 pub fn write_handshake<W: Write>(
@@ -13,10 +15,10 @@ pub fn write_handshake<W: Write>(
     next_state: i32,
 ) -> Result<()> {
     write_packet(w, 0x00, |w| {
-        write_var_int(w, protocol_version)?;
+        varint::write(w, protocol_version)?;
         write_string(w, host)?;
         write_unsigned_short(w, port)?;
-        write_var_int(w, next_state)?;
+        varint::write(w, next_state)?;
         Ok(())
     })
 }
@@ -43,10 +45,10 @@ where
 {
     let buf = &mut Vec::new();
 
-    write_var_int(buf, id)?;
+    varint::write(buf, id)?;
     f(buf)?;
 
-    write_var_int(w, buf.len() as i32)?;
+    varint::write(w, buf.len() as i32)?;
     w.write_all(buf)?;
 
     Ok(())
@@ -57,12 +59,12 @@ where
     R: Read,
     F: FnOnce(i32, &mut dyn Read) -> Result<T>,
 {
-    let len = read_var_int(r)? as usize;
+    let len = varint::read(r)? as usize;
     let mut data = vec![0; len];
     r.read_exact(&mut data)?;
     let mut data = Cursor::new(data);
 
-    let packet_id = read_var_int(&mut data)?;
+    let packet_id = varint::read(&mut data)?;
     let value = f(packet_id, &mut data)?;
 
     Ok(value)
@@ -74,13 +76,13 @@ pub fn write_unsigned_short<W: Write + ?Sized>(w: &mut W, value: u16) -> Result<
 }
 
 pub fn write_string<W: Write + ?Sized>(w: &mut W, value: &str) -> Result<()> {
-    write_var_int(w, value.len() as i32)?;
+    varint::write(w, value.len() as i32)?;
     w.write_all(value.as_bytes())?;
     Ok(())
 }
 
 pub fn read_string<R: Read + ?Sized>(r: &mut R) -> Result<String> {
-    let len = read_var_int(r)? as usize;
+    let len = varint::read(r)? as usize;
     let mut buf = vec![0; len];
 
     r.read_exact(&mut buf)?;
@@ -90,51 +92,8 @@ pub fn read_string<R: Read + ?Sized>(r: &mut R) -> Result<String> {
     Ok(value)
 }
 
-const SEGMENT_BITS: u32 = 0x7F;
-const CONTINUE_BIT: u32 = 0x80;
-
-pub fn write_var_int<W: Write + ?Sized>(w: &mut W, value: i32) -> Result<()> {
-    let mut value = value as u32;
-
-    loop {
-        if (value & !SEGMENT_BITS) == 0 {
-            w.write_all(&[value as u8])?;
-            return Ok(());
-        }
-
-        let segment = (value & SEGMENT_BITS) | CONTINUE_BIT;
-
-        w.write_all(&[segment as u8])?;
-
-        value >>= 7;
-    }
-}
-
 pub fn read_byte<R: Read + ?Sized>(r: &mut R) -> Result<u8> {
     let buf = &mut [0];
     r.read_exact(buf)?;
     Ok(buf[0])
-}
-
-pub fn read_var_int<R: Read + ?Sized>(r: &mut R) -> Result<i32> {
-    let mut value: u32 = 0;
-    let mut position = 0;
-
-    loop {
-        let current_byte = read_byte(r)? as u32;
-
-        value |= (current_byte & SEGMENT_BITS) << position;
-
-        if (current_byte & CONTINUE_BIT) == 0 {
-            break;
-        }
-
-        position += 7;
-
-        if position >= 32 {
-            bail!("VarInt is too big");
-        }
-    }
-
-    Ok(value as i32)
 }
---
+++ src/protocol/varint.rs
@@ -0,0 +1,47 @@
+use std::io::{Read, Write};
+
+use super::read_byte;
+use anyhow::{bail, Result};
+
+const SEGMENT_BITS: u32 = 0x7F;
+const CONTINUE_BIT: u32 = 0x80;
+
+pub fn read<R: Read + ?Sized>(r: &mut R) -> Result<i32> {
+    let mut value: u32 = 0;
+    let mut position = 0;
+
+    loop {
+        let current_byte = read_byte(r)? as u32;
+
+        value |= (current_byte & SEGMENT_BITS) << position;
+
+        if (current_byte & CONTINUE_BIT) == 0 {
+            break;
+        }
+
+        position += 7;
+
+        if position >= 32 {
+            bail!("VarInt is too big");
+        }
+    }
+
+    Ok(value as i32)
+}
+
+pub fn write<W: Write + ?Sized>(w: &mut W, value: i32) -> Result<()> {
+    let mut value = value as u32;
+
+    loop {
+        if (value & !SEGMENT_BITS) == 0 {
+            w.write_all(&[value as u8])?;
+            return Ok(());
+        }
+
+        let segment = (value & SEGMENT_BITS) | CONTINUE_BIT;
+
+        w.write_all(&[segment as u8])?;
+
+        value >>= 7;
+    }
+}
